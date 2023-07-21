04812bd81760ccbca63aa39baad344ddc36f73b8
Sayvai <sayvai@hotmail.com>
2023-07-21 16:55:33 +0100

    chore: add drf, views, and serializers
    drf = django rest framework (djangorestframework)
    
    created view classes for menu items (Menu) and bookings (Booking)
    
    created serializers for the Menu and Booking model

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("menu/", views.MenuItemsView.as_view()),
+    path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
+    path("booking/table", views.BookingView.as_view()),
+    path("booking/table/<int:pk>", views.SingleBookingView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class SingleBookingView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
