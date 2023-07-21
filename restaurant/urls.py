31026465ad69c24a179d2bed1a129ae78da89efe
Sayvai <sayvai@hotmail.com>
2023-07-21 22:00:55 +0100

    chore: secure apis with authentication

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
@@ -8,6 +9,7 @@ router.register(r"table", views.BookingViewSet)
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api-token-auth/", obtain_auth_token),
     path("menu/", views.MenuItemsView.as_view()),
     path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
     path("booking/", include(router.urls)),
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
@@ -14,12 +15,27 @@ class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != "GET":
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != "GET":
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
