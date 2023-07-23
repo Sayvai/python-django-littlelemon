078ff5e0b691b64de4c6e054fa47089250e3f13d
Sayvai <sayvai@hotmail.com>
2023-07-23 10:53:01 +0100

    chore: refactor booking model to associate with registered user
    Replaces the name field with a user_id as the foreign key.
    
    This should allow only registered users to manage table bookings online through authenticated CRUD operations.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -12,7 +13,7 @@ class Menu(models.Model):
 
 
 class Booking(models.Model):
-    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
 
