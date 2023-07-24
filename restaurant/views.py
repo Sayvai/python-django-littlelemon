b579e58cb49422f6af9ac4542a07f00489d97872
Sayvai <sayvai@hotmail.com>
2023-07-24 12:06:58 +0100

    feat: manage bookings in frontend html page

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework import generics, viewsets, permissions, status
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Menu, Booking
@@ -39,6 +40,12 @@ def signup(request):
     return render(request, "register.html")
 
 
+class IsManager(permissions.BasePermission):
+    # override the has_permission method
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="Manager").exists()
+
+
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
@@ -46,7 +53,7 @@ class MenuItemsView(generics.ListCreateAPIView):
     def get_permissions(self):
         permission_classes = []
         if self.request.method != "GET":
-            permission_classes = [IsAuthenticated]
+            permission_classes = [IsAuthenticated, IsManager]
 
         return [permission() for permission in permission_classes]
 
@@ -58,7 +65,7 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     def get_permissions(self):
         permission_classes = []
         if self.request.method != "GET":
-            permission_classes = [IsAuthenticated]
+            permission_classes = [IsAuthenticated, IsManager]
 
         return [permission() for permission in permission_classes]
 
@@ -67,3 +74,38 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = Booking.objects.filter(user=request.user)
+        serializer = BookingSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        request.data["user"] = request.user.id
+        booking = Booking(
+            user=request.user,
+            booking_date=request.data["booking_date"],
+            no_of_guests=request.data["no_of_guests"],
+        )
+        booking.save()
+
+        return Response(
+            {"message": "Booking created successfully"},
+            status=status.HTTP_201_CREATED,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        booking = Booking.objects.get(pk=kwargs["pk"])
+        if (
+            booking.user != request.user
+            or not request.user.groups.filter(name="Manager").exists()
+        ):
+            booking.delete()
+            return Response(
+                {"message": "Booking deleted successfully"},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        return Response(
+            {"message": "You cannot delete this booking"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
